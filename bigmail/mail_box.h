597ae6fa67d979c8eb18bca0d357c371e1bd814c
recyclingben <bdcubbage@gmail.com>
2020-03-29 19:28:54 -0400

    Add concept for mailbox entries.

--- bigmail/mail_box.h
+++ bigmail/mail_box.h
@@ -1,8 +1,20 @@
 #pragma once
 #include <stdlib.h>
 
+typedef enum {
+    mail_box_ENTRY_DEAD = 0,
+    mail_box_ENTRY_LIVE = 1,
+    mail_box_ENTRY_LOCK = 2
+} mail_box_MailBoxEntryState;
+
+typedef struct {
+    _Atomic mail_box_MailBoxEntryState state;
+} mail_box_MailBoxEntryHead;
+
 typedef struct {
     int content_sizeof;
+    mail_box_MailBoxEntryHead *_Atomic lagging;
+    mail_box_MailBoxEntryHead *_Atomic leading;
 } mail_box_MailBoxHead;
 
 void mail_box_create(int content_sizeof,
@@ -13,4 +25,6 @@ void mail_box_create(int content_sizeof,
                      mail_box_MailBoxHead **out_mailbox)
 {
     *out_mailbox = calloc(1, sizeof(mail_box_MailBoxHead) + content_sizeof);
+    (*out_mailbox)->lagging = (mail_box_MailBoxEntryHead *)(*out_mailbox + 1);
+    (*out_mailbox)->leading = (mail_box_MailBoxEntryHead *)(*out_mailbox + 1);
 }
