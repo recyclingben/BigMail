ed9e1890147036b98d7de9ac7aaabd84265e306b
recyclingben <bdcubbage@gmail.com>
2020-03-30 16:57:45 -0400

    Entries no longer maintain possible lock state; Revise algo to increment lead atomically; Add some wrapper functions.

--- bigmail/mail.h
+++ bigmail/mail.h
@@ -4,3 +4,8 @@
 
 typedef mail_sys_MailSys     mail_MailSys;
 typedef mail_box_MailBoxHead mail_MailBox;
+
+#define mailbox_incoming_open(mail_system, content) mail_box_incoming_open(mail_system, (void **)content)
+#define mailbox_incoming_shut(mail_system, content) mail_box_incoming_shut(mail_system, *content)
+#define mailbox_outgoing_open(mail_system, content) mail_box_outgoing_open(mail_system, (void **)content)
+#define mailbox_outgoing_shut(mail_system, content) mail_box_outgoing_shut(mail_system, *content)
--- bigmail/mail_box.h
+++ bigmail/mail_box.h
@@ -1,18 +1,9 @@
 #pragma once
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
 
-#define offset(of, by) ((void *)((char *)(of) + (by)))
-
-typedef enum {
-    mail_box_ENTRY_DEAD = 0,
-    mail_box_ENTRY_LIVE = 1,
-    mail_box_ENTRY_LOCK = 2
-} mail_box_MailBoxEntryState;
-
-typedef struct {
-    _Atomic mail_box_MailBoxEntryState state;
-} mail_box_MailBoxEntryHead;
+#define offset(of, by) ((void *)((char *)(of) + (int)(by)))
 
 typedef struct {
     int content_sizeof;
@@ -20,43 +11,87 @@ typedef struct {
     uint16_t _Atomic leading;
 } mail_box_MailBoxHead;
 
+typedef struct {
+    _Atomic bool live;
+} mail_box_MailBoxEntryHead;
+
 void mail_box_create(int content_sizeof,
                      mail_box_MailBoxHead **out_mailbox);
 
 void mail_box_incoming_open(mail_box_MailBoxHead *mailbox,
                             void **out_content);
 
-void mail_box_incoming_shut(mail_box_MailBoxHead *mailbox);
+void mail_box_incoming_shut(mail_box_MailBoxHead *mailbox,
+                            void *content);
+
+void mail_box_outgoing_open(mail_box_MailBoxHead *mailbox,
+                            void **out_content);
+
+void mail_box_nth_entry(mail_box_MailBoxHead *mailbox,
+                        int n,
+                        mail_box_MailBoxEntryHead **out_entry);
+
+void mail_box_nth_content(mail_box_MailBoxHead *mailbox,
+                          int n,
+                          void **out_content);
 
 
 void mail_box_create(int content_sizeof,
                      mail_box_MailBoxHead **out_mailbox)
 {
-    int mailbox_sizeof = 0;
-    mailbox_sizeof += content_sizeof + sizeof(mail_box_MailBoxEntryHead);
-    mailbox_sizeof *= (uint16_t)~0;
-    mailbox_sizeof += sizeof(mail_box_MailBoxHead);
-
-    *out_mailbox = calloc(1, mailbox_sizeof);
+    *out_mailbox = calloc(1, sizeof(mail_box_MailBoxHead)
+        + ((uint16_t)~0 + 1) * (content_sizeof + sizeof(mail_box_MailBoxEntryHead)));
     (*out_mailbox)->content_sizeof = content_sizeof;
 }
 
 void mail_box_incoming_open(mail_box_MailBoxHead *mailbox,
                             void **out_content)
 {
-    int content_offset = sizeof(mail_box_MailBoxEntryHead) + mailbox->content_sizeof;
-    content_offset *= mailbox->lagging;
-    content_offset += sizeof(mail_box_MailBoxHead);
-
-    mail_box_MailBoxEntryHead *entry
-            = offset(mailbox, content_offset - sizeof(mail_box_MailBoxEntryHead));
+    mail_box_MailBoxEntryHead *entry;
+    mail_box_nth_entry(mailbox, mailbox->lagging, &entry);
 
     *out_content = NULL;
-    if (entry->state == mail_box_ENTRY_LIVE)
-        *out_content = offset(mailbox, content_offset);
+    if (entry->live)
+        *out_content = offset(entry, sizeof(mail_box_MailBoxEntryHead));
+
+    entry->live = false;
+    ++mailbox->lagging;
 }
 
-void mail_box_incoming_shut(mail_box_MailBoxHead *mailbox)
+void mail_box_incoming_shut(mail_box_MailBoxHead *mailbox,
+                            void *content)
 {
-    ++mailbox->lagging;
+    /* This is only here for symmetry with outgoing operations. */
+}
+
+void mail_box_outgoing_open(mail_box_MailBoxHead *mailbox,
+                            void **out_content)
+{
+    uint16_t n = __atomic_add_fetch(&mailbox->leading, 1, __ATOMIC_RELAXED) - 1;
+    mail_box_nth_content(mailbox, n, out_content);
+}
+
+void mail_box_outgoing_shut(mail_box_MailBoxHead *mailbox,
+                            void *content)
+{
+    mail_box_MailBoxEntryHead *entry
+        = offset(content, -sizeof(mail_box_MailBoxEntryHead));
+    entry->live = true;
+}
+
+void mail_box_nth_entry(mail_box_MailBoxHead *mailbox,
+                        int n,
+                        mail_box_MailBoxEntryHead **out_entry)
+{
+    *out_entry = offset(mailbox, sizeof(mail_box_MailBoxHead)
+        + n * (sizeof(mail_box_MailBoxEntryHead) + mailbox->content_sizeof));
+}
+
+void mail_box_nth_content(mail_box_MailBoxHead *mailbox,
+                          int n,
+                          void **out_content)
+{
+    *out_content = offset(mailbox, sizeof(mail_box_MailBoxHead)
+        + n * (sizeof(mail_box_MailBoxEntryHead) + mailbox->content_sizeof)
+        + sizeof(mail_box_MailBoxEntryHead));
 }
