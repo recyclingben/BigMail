5f87e0ce16f503e7ca391553ee02f3f0a133c3c3
recyclingben <bdcubbage@gmail.com>
2020-03-29 20:32:32 -0400

    Allow open/shut retrival and release for incoming mailboxes.

--- bigmail/mail_box.h
+++ bigmail/mail_box.h
@@ -1,5 +1,8 @@
 #pragma once
+#include <stdint.h>
 #include <stdlib.h>
+
+#define offset(of, by) ((void *)((char *)(of) + (by)))
 
 typedef enum {
     mail_box_ENTRY_DEAD = 0,
@@ -13,16 +16,47 @@ typedef struct {
 
 typedef struct {
     int content_sizeof;
-    int lagging;
-    int _Atomic leading;
+    uint16_t lagging;
+    uint16_t _Atomic leading;
 } mail_box_MailBoxHead;
 
 void mail_box_create(int content_sizeof,
                      mail_box_MailBoxHead **out_mailbox);
 
+void mail_box_incoming_open(mail_box_MailBoxHead *mailbox,
+                            void **out_content);
+
+void mail_box_incoming_shut(mail_box_MailBoxHead *mailbox);
+
 
 void mail_box_create(int content_sizeof,
                      mail_box_MailBoxHead **out_mailbox)
 {
-    *out_mailbox = calloc(1, sizeof(mail_box_MailBoxHead) + content_sizeof);
+    int mailbox_sizeof = 0;
+    mailbox_sizeof += content_sizeof + sizeof(mail_box_MailBoxEntryHead);
+    mailbox_sizeof *= (uint16_t)~0;
+    mailbox_sizeof += sizeof(mail_box_MailBoxHead);
+
+    *out_mailbox = calloc(1, mailbox_sizeof);
+    (*out_mailbox)->content_sizeof = content_sizeof;
+}
+
+void mail_box_incoming_open(mail_box_MailBoxHead *mailbox,
+                            void **out_content)
+{
+    int content_offset = sizeof(mail_box_MailBoxEntryHead) + mailbox->content_sizeof;
+    content_offset *= mailbox->lagging;
+    content_offset += sizeof(mail_box_MailBoxHead);
+
+    mail_box_MailBoxEntryHead *entry
+            = offset(mailbox, content_offset - sizeof(mail_box_MailBoxEntryHead));
+
+    *out_content = NULL;
+    if (entry->state == mail_box_ENTRY_LIVE)
+        *out_content = offset(mailbox, content_offset);
+}
+
+void mail_box_incoming_shut(mail_box_MailBoxHead *mailbox)
+{
+    ++mailbox->lagging;
 }
